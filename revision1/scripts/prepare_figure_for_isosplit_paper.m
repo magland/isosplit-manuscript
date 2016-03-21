1f7a44d8798ade3c494fc8c931ed805988d445b1
Jeremy Magland <magland@jm02.scdanet.org>
2016-03-21 11:33:08 -0400

    real data figure

---
+++ revision1/scripts/prepare_figure_for_isosplit_paper.m
@@ -0,0 +1,45 @@
+function prepare_figure_for_isosplit_paper
+
+close all;
+
+clip_size=200;
+labels_to_view=[4,6,8,10,11];
+
+mfile_path=fileparts(mfilename('fullpath'));
+path0=[mfile_path,'/sort_dl12_20151208_NNF_r1_tet16_17/output_tet16'];
+firings=readmda([path0,'/firings.mda']);
+outlier_scores=firings(5,:);
+inds=find(outlier_scores<=3);
+firings=firings(:,inds);
+K=max(firings(3,:));
+labels_map=zeros(1,K);
+labels_map(labels_to_view)=1:length(labels_to_view);
+labels=firings(3,:);
+labels=labels_map(labels);
+firings(3,:)=labels;
+inds=find(labels>0);
+firings=firings(:,inds);
+times=firings(2,:);
+labels=firings(3,:);
+
+writemda64(firings,[mfile_path,'/firings_for_isosplit_paper.mda']);
+fprintf('Reading raw...\n');
+pre2=readmda([path0,'/pre2.mda']);
+fprintf('Extracting clips...\n');
+clips=ms_extract_clips2(pre2,times,clip_size);
+fprintf('ms_templates...\n');
+templates=ms_templates(clips,labels);
+
+aa=randsample(length(times),1000);
+FF=ms_event_features(clips,3);
+
+figure;
+subplot(1,2,1);
+ms_view_clusters(FF,labels);
+xlabel(''); ylabel(''); zlabel('');
+set(gca,'xtick',[]); set(gca,'ytick',[]); set(gca,'ztick',[]);
+subplot(1,2,2);
+ms_view_templates(templates);
+set(gcf,'position',[450,1300,1300,450]);
+
+end
