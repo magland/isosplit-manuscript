97b1f5740440e65f2fea47ffc485fb8640029cc3
Jeremy Magland <magland@jm02.scdanet.org>
2016-03-21 11:53:50 -0400

    update figure

--- revision1/scripts/FIG05_nongaussian_histogram.m
+++ revision1/scripts/FIG05_nongaussian_histogram.m
@@ -8,7 +8,8 @@ set(gcf,'Color','w');
 set(gca,'xtick',[])
 set(gca,'ytick',[])
 
-set(gcf,'paperposition',[0,0,5,3.5]);
-print('../images/nongaussian_histogram.eps','-depsc2');
+% Save .eps by hand
+%set(gcf,'paperposition',[0,0,5,3.5]);
+%print('../images/nongaussian_histogram.eps','-depsc2');
 
 end
